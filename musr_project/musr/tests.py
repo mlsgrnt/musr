8ca07439cf391d8d33ac8b9122c98b1fdaa3ca81
Miles Grant <mlsgrnt@gmail.com>
2019-02-22 10:42:23 +0000

    Add unit tests, travis
    Fix travis
    
    fix travis (again)
    
    fix travis (again) (again)
    
    Fix travis one more time

--- musr_project/musr/tests.py
+++ musr_project/musr/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TravisTesterTestCase(TestCase):
+    def test_unit_tests_are_understood_and_can_pass(self):
+        """Unit tests run and are able to pass"""
+        test_value = 5
+        self.assertEqual(test_value, 5)
