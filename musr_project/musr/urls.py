41363725b797d2735587b811a2ab0251996fb236
Scott Isaac <2419523I@student.gla.ac.uk>
2019-02-28 23:15:11 +0000

    configuring dummy views
    created and configured the following views What’s hot,Profile and Feed. These were configured to very basic templates.

--- musr_project/musr/urls.py
+++ musr_project/musr/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
-urlpatterns = [path("", views.test_page)]
+urlpatterns = [
+    path("", views.whats_hot, name="whats_hot"),
+    path("feed/", views.feed, name="feed"),
+    # TODO:
+    # How to match to profile/username/
+    path("profile/", views.profile, name="profile"),
+]
--- musr_project/musr/views.py
+++ musr_project/musr/views.py
@@ -3,6 +3,18 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 # Create your views here.
+
+# Index view (Whats hot)
+def whats_hot(request):
+    return render(request, "musr/whats_hot.html", {"user": request.user})
+
+
+# Profile
 @login_required
-def test_page(request):
-    return render(request, "musr/test.html", {"user": request.user})
+def profile(request):
+    return render(request, "musr/profile.html", {"user": request.user})
+
+
+@login_required
+def feed(request):
+    return render(request, "musr/feed.html", {"user": request.user})
