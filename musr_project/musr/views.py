3500772df22625188c27588de0dbc15a1e5049bb
Scott Isaac <2419523I@student.gla.ac.uk>
2019-03-01 09:02:33 +0000

    fixing links from base

--- musr_project/musr/views.py
+++ musr_project/musr/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-# Create your views here.
 
 # Index view (Whats hot)
 def whats_hot(request):
     return render(request, "musr/whats_hot.html", {"user": request.user})
 
 
-# Profile
+# Does profile require login?
 @login_required
 def profile(request):
     return render(request, "musr/profile.html", {"user": request.user})
 
 
+# Feed view
 @login_required
 def feed(request):
     return render(request, "musr/feed.html", {"user": request.user})
