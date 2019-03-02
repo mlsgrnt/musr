bb227fadf763c58b1e983be56adcf0f06ffb870b
Callum Hunter <2247415h@student.gla.ac.uk>
2019-03-02 21:30:40 +0000

    added feed functionallity
    now a user's feed will show all posts that were made by people they follow

--- musr_project/musr/admin.py
+++ musr_project/musr/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Post
+from .models import Profile, Post, Following
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Post)
+admin.site.register(Following)
