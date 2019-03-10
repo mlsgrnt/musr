4f09376be1fe9b9ed9374dfcae2a50a5679dd17a
Callum Hunter <2247415h@student.gla.ac.uk>
2019-03-10 15:26:14 +0000

    fixed 404 issues

--- musr_project/musr/views.py
+++ musr_project/musr/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import redirect
 
 from .models import Profile, Post, Following
