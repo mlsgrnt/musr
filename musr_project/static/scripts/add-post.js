f6167c1891263744f9626a5a99217b767ac298d6
Miles Grant <mlsgrnt@gmail.com>
2019-03-20 18:26:59 +0000

    Comment and finalize add-post.js

--- musr_project/static/scripts/add-post.js
+++ musr_project/static/scripts/add-post.js
@@ -24,18 +24,24 @@ function hookForm() {
 window.onload = hookForm;
 
 const closeAddPostForm = e => {
+  // Make the form dissapear if the escape key was pressed or no key was
+  // pressed, suggesting the close button was clicked
   if (!e.keyCode || e.keyCode == 27) {
     document.querySelector('.container').classList.remove('addingPost');
   }
 };
 
 const addPostButtonClickHandler = e => {
-  document.querySelector('.songSearch').focus();
+  // Make the add post interface appear, and focus the input field
+  // so typing can begin right away
   document.querySelector('.container').classList.add('addingPost');
+  document.querySelector('.songSearch').focus();
 };
 
 const searchFieldChangeHandler = e => {
+  // A new search is ready to fire, so abort the old one
   controller.abort();
+  // Fire off new search
   updateResults(e.target.value);
 };
 
@@ -44,32 +50,42 @@ const selectSong = e => {
 
   // Browser compatibility
   const path = e.path || (e.composedPath && e.composedPath());
-  // traverse tree of clicked elements to find the li so we can grab the ID
+
+  // Traverse tree of clicked elements to find the li so we can grab the ID
   const songId = path.find(element => {
     // Find LI element and check it has an id just to be safe
     return element.nodeName == 'LI' && element.id;
   }).id;
 
+  // Populate hidden form with song id
   const input = document.querySelector('.songIdInput');
   input.value = songId;
 
+  // Grab url to POST to from hidden form
   const form = document.querySelector('.addPostForm');
   const url = form.action;
 
   fetch(url, {
     method: 'POST',
     body: new FormData(form)
-  }).then(() => {
-    // Success!
-    form.reset();
+  }).then(async response => {
+    const body = await response;
+    const responseText = await body.text();
 
-    document.querySelector('.songSearchResults').innerHTML = 'Song added!';
+    // Get ready for next song
+    form.reset();
     document.querySelector('.songSearch').value = '';
+
+    // Replace song list with message
+    document.querySelector('.songSearchResults').innerHTML =
+      responseText == 'OK'
+        ? 'Song added!'
+        : 'Failed to add song! Please try again.';
 
     // Remove ourselves
     window.setTimeout(() => {
       document.querySelector('.container').classList.remove('addingPost');
-    }, 750);
+    }, 1000);
   });
 };
 
@@ -78,8 +94,10 @@ const updateResults = query => {
   // Move up search box in anticipation
   songSearchResults.classList.add('loaded');
 
+  // Give user feedback
   songSearchResults.innerHTML = 'Loading...';
 
+  // Initiate request
   fetch(`https://deezer-proxy.glitch.me/search?q="${query}"`, {
     singal
   }).then(async result => {
