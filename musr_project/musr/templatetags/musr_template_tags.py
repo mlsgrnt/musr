aa3d8993c4bc7fa1a23b52e470a2bb032adc27a1
Miles Grant <mlsgrnt@gmail.com>
2019-03-20 16:15:53 +0000

    Fix template tag context check

--- musr_project/musr/templatetags/musr_template_tags.py
+++ musr_project/musr/templatetags/musr_template_tags.py
@@ -11,7 +11,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def current(context, url=None):
     if (
-        "resolver_match" in context.request
+        not 'exception' in context
         and context.request.resolver_match.url_name == url
     ):
         return "active"
